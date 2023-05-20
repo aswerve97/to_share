ee23d2bb3447733b48a8123d8c07869edf25baf8
Anonymous <74816568+aswerve97@users.noreply.github.com>
2023-05-20 15:00:06 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,45 @@
+import discord
+from logic import idk
+from idk2 import getUserFromMention
+import json
+
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print("We have logged in as {0.user}".format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("$hello"):
+        await message.channel.send(message.author)
+
+    if message.content.startswith("$based"):
+        text = idk(str(message.author))
+        await message.channel.send(text)
+    
+
+    if message.content.endswith("$based"):
+        dirt_text = str(message.mentions)
+        print(dirt_text)
+        name = dirt_text.split()[2].replace("name=","")
+        text = idk(name)
+        await message.channel.send(text)
+
+    if message.content.endswith("$based leaderboard"):
+        with open("users.json","r") as file:
+            data = json.load(file)
+        await message.channel.send(data)
+    
+
+    if message.content == "!stop":
+        await client.logout()
+
+
+client.run("OTE1NDY2MDM2NTkyMTE5ODE5.YacALw.NaSIZQlpm0ZAhw4xuR2Tk7EzSiY")
